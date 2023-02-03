76e11b5d74546d9c996bf52157037a08929fa15f
Anton Deguet <anton.deguet@jhu.edu>
2023-02-02 20:25:00 -0500

    setup.py: copy scripts to build tree so we can use them with ros2 run

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
-from setuptools import setup
+import os
+import glob
+import setuptools
 
 package_name = 'dvrk_python'
 
-setup(
-    name=package_name,
-    version='1.0.0',
-    packages=['dvrk'],
-    package_dir={'': 'src'},
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+setuptools.setup(
+    name = package_name,
+    version = '0.0.0',
+    packages = ['dvrk'],
+    package_dir = {'': 'src'},
+    data_files = [
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-    ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='Anton Deguet',
-    maintainer_email='anton.deguet@jhu.edu',
-    description='dVRK Python client for ROS2',
-    license='MIT',
-    entry_points={
+        ('lib/' + package_name, glob.glob('scripts/*.py')),
+     ],
+    install_requires = ['setuptools'],
+    zip_safe = True,
+    maintainer = 'Anton Deguet',
+    maintainer_email = 'anton.deguet@jhu.edu',
+    description = 'dVRK Python client for ROS2',
+    license = 'MIT',
+    entry_points = {
         'console_scripts': [
         ],
     },
