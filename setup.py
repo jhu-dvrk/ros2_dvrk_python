d6461c31d29af8b7c3c7b7e78285c69d69965926
Ketan13294 <mketan92@gmail.com>
2023-04-18 21:40:30 -0400

    Changed setup.py to add the test scripts on ros2 run path

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     data_files = [
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('scripts/' + package_name, glob('scripts/*.py')),
+        ('lib/' + package_name, glob('scripts/*.py')),
     ],
     install_requires = ['setuptools'],
     zip_safe = True,
